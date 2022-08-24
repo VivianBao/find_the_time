b6829e797657337598b75725ceed1c9e312ca8f6
Vivian Bow <vivianbao1009@gmail.com>
2022-08-24 20:19:36 +0900

    Modified foreign key relationships in migration files

--- db/migrate/20220824110435_create_friendships.rb
+++ db/migrate/20220824110435_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :friend, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: true
 
       t.timestamps
     end
