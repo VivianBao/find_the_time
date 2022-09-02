224d436dd78ad830c23bb02b7f72d7bd9feefda0
Vivian Bow <vivianbao1009@gmail.com>
2022-09-02 22:59:47 +0900

    Imported devise, add devise to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
       get 'huddles/index'
@@ -9,5 +10,17 @@ Rails.application.routes.draw do
   end
 
   root 'homepage#index'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'user/:id', to: 'users/registrations#detail'
+    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
